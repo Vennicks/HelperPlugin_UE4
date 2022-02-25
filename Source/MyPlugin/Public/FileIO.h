27075f587092bf39e213f5fdfffe2e82f784e190
Vennicks <gauzarguesvincent@gmail.com>
2022-02-25 10:37:07 +0100

    Initialization Plugin

---
+++ Source/MyPlugin/Public/FileIO.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "FileIO.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPLUGIN_API UFileIO : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+};
